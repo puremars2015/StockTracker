3daa39803c4fade7b1183dafba7b3f8304cbc0db
puremars2015 <puremars2k10@gmail.com>
2022-05-01 13:33:04 +0800

    Update
    新增主畫面,登入畫面,註冊畫面,修改模板部分規格

--- stock_tracking_system/project/templete.js
+++ stock_tracking_system/project/templete.js
@@ -12,13 +12,16 @@ function GetPageRowHTML(n) {
                                     <button id="qbtn${n}" class="finger" onclick="Query(this)" value="${n}">查詢個股資料</button>
                                     <!-- input id="no${n}" type="text" value="${n}"  hidden -->
                                 </div>
+
                                 <div>
                                     <label class="lb">個股代號(例如:台積電2330):</label>
                                     <input id="scode${n}" class="condition" type="text" placeholder="ex:2330">
                                 </div>
+
                                 <div>
-                                    <label class="lb">查詢區間(例如:由今天開始向前一個月,輸入1mo):</label>
+                                    <label class="lb">資料粒度(例如:由今天開始向前一個月,輸入1mo):</label>
                                     <div style="margin-bottom:10px">
+                                        <button name="period${n}" class="finger" value="${n}" onclick="ChangePeriod(this)">now</button>
                                         <button name="period${n}" class="finger" value="${n}" onclick="ChangePeriod(this)">1d</button>
                                         <button name="period${n}" class="finger" value="${n}" onclick="ChangePeriod(this)">5d</button>
                                         <button name="period${n}" class="finger" value="${n}" onclick="ChangePeriod(this)">1mo</button>
@@ -27,7 +30,13 @@ function GetPageRowHTML(n) {
                                         <input type="text "id="qdate${n}" class="condition" type="text" value="1mo" readonly>
                                     </div>
                                   
-                                </div>          
+                                </div>
+
+                                <div>
+                                    <label class="lb">查詢範圍(輸入起訖日期):</label>
+                                    <input type="text">~<input type="text">
+                                </div>       
+
                                 <div>
                                     <label class="lb">模糊化係數(例如:30,表示以30為一格,減少小波動):</label>
                                     <input id="mosiac${n}" class="condition" type="text" placeholder="ex:30">
